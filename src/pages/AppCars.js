ec78f0fbd0552878708f6f5c54a5a42ff92536a6
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-8.local>
2022-03-22 18:23:03 +0100

    zadatak 2

--- src/pages/AppCars.js
+++ src/pages/AppCars.js
@@ -1,12 +1,31 @@
-
+import carService from '../services/CarsService';
+import React, { useState } from 'react';
+// import {Link} from 'react-router-dom';
 
 function AppCars() {
-    return (
 
-<p>pages/AppCars.js <br></br>
-    Default page;
-</p>
-    );
+    const [cars, setCars] = useState(carService.getAll())
+
+    return (
+        <div>
+            <ul>
+        {cars.map((car) =>
+        <li style={{ border: '1px solid black', marginBottom: '5px', padding: 5, display: 'flex', flexDirection: 'column'}} key={car.id}>
+          <span>Brand: {car.brand}</span>
+          <span>Model: {car.model}</span>
+          <span>Year: {car.year}</span>
+          <span>Max speed: {car.maxSpeed}</span>
+          <span>Automatic: {car.isAutomatic}</span>
+          <span>Engine: {car.engine}</span>
+          <span>Doors: {car.numberOfDoors}</span>
+
+          {/* <Link to={`/cars/${car.id}`}>Latest Purchase</Link> */}
+          {/* <button onClick={() => deleteCar(car.id)}>Delete</button> */}
+        </li>)}
+      </ul>
+        </div>
+      )
 }
 
 export default AppCars;
+// id, brand, model, year, maxSpeed, isAutomatic, engine, numberOfDoors.
--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -13,13 +13,38 @@ class CarService {
   async getAll() {
       try {
         const {data} = await this.client.get('api/cars');
-        return data
+        return data;
       } catch (error) {
         console.log('Greska', error)
       }
 
       return []
   }
+
+  delete(id) {
+    const index = this.cars.findIndex((car) => car.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this.cars.splice(index, 1);
+
+    return true
+  }
+
+  create(newCar) {
+      this.cars.push({id: this.nextId, ...newCar, products: []});
+
+      this.nextId++;
+
+      return this.cars[this.cars.length-1];
+  }
+
+  get(id) {
+    return this.cars.find(c => c.id === Number(id))
+  }
+
 }
 
 export default new CarService()
